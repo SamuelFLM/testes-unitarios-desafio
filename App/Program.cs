64efd79f6536cc8ff72a40a3b77c7d116a28eed3
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-29 11:47:03 -0300

    add: escopo

--- App/Program.cs
+++ App/Program.cs
@@ -1,2 +1,7 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+List<int> numeros = new List<int> { 1, 2, 3};
+
+var lista = numeros.Select(x => x + 1);
+
+foreach (var i in lista)
+    Console.WriteLine(i);
---
+++ App/Services/ValidacoesLista.cs
@@ -0,0 +1,39 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace App.Services
+{
+    public class ValidacoesLista
+    {
+        public List<int> RemoverNumerosNegativos(List<int> lista)
+        {
+            var listaSemNegativos = lista.Where(x => x > 0);
+            return listaSemNegativos.ToList();
+        }
+        public bool ListaContemDeterminadoNumero(List<int> lista, int numero)
+        {
+            bool resultado = lista.Contains(numero);
+            return resultado;
+        }
+
+        public List<int> MultiplicaNumerosLista(List<int> lista, int numero)
+        {
+            var listaMultiplicada = lista.Select(x => x * numero).ToList();
+            return listaMultiplicada;
+        }
+
+        public int RetornarMaiorNumeroLista(List<int> numeros)
+        {
+            return numeros.Max();
+        }
+        public int RetornarMenorNumeroLista(List<int> numeros)
+        {
+            return numeros.Min();
+        }
+
+
+    }
+}
