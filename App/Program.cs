3f0da951dd202979c275bcb1a384a71ea438b1b8
Samuel Felipe Lima <felipelimaaa23@gmail.com>
2023-09-29 13:01:12 -0300

    fix: testes de string concluidos.

--- App/Program.cs
+++ App/Program.cs
@@ -1,7 +1,2 @@
 ﻿
-List<int> numeros = new List<int> { 1, 2, 3};
-
-var lista = numeros.Select(x => x + 1).ToList();
-
-Console.WriteLine(lista[1]);
-
+Console.WriteLine("Hello, world.");
--- Tests/Services/ValidacoesStringTests.cs
+++ Tests/Services/ValidacoesStringTests.cs
@@ -2,16 +2,72 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using App.Services;
 using Xunit;
 
 namespace Tests.Services
 {
     public class ValidacoesStringTests
     {
-        [Fact]
-        public void Test1()
+        private ValidacoesString _string;
+        public ValidacoesStringTests()
         {
-            Assert.True(true);
+            _string = new ValidacoesString();
+        }
+
+        [Fact]
+        public void DeveRetornar6QuantidadeCaracteresDaPalavraMatrix()
+        {
+            string texto = "Matrix";
+            int resultado = _string.RetornarQuantidadeCaracteres(texto);
+            Assert.Equal(6, resultado);
+        }
+        [Fact]
+        public void DeveConterAPalavraQualquerNoTexto()
+        {
+            // Given
+            string texto = "Esse é um texto qualquer";
+            string textoProcurado = "qualquer";
+            // When
+            bool resultado = _string.ContemCaractere(texto, textoProcurado);
+            // Then
+            Assert.True(resultado);
+        }
+
+        [Fact]
+        public void NaoDeveConterAPalavraNoTexto()
+        {
+            // Given
+            string texto = "Esse é um texto qualquer";
+            string textoProcurado = "tests";
+            // When
+            bool resultado = _string.ContemCaractere(texto, textoProcurado);
+            // Then
+            Assert.False(resultado);
+        }
+
+        [Fact]
+        public void TextoDeveTerminarComAPalavraProcurado()
+        {
+            // Given
+            string texto = "Começo, meio e fim do texto procurado";
+            string textoFinalizaCom = "procurado";
+            // When
+            bool resultado = _string.TextoTerminaCom(texto, textoFinalizaCom);
+            // Then
+            Assert.True(resultado);
+        }
+
+        [Fact]
+        public void TextoNaoTerminarComAPalavraProcurado()
+        {
+            // Given
+            string texto = "Começo, meio e fim do texto procurado";
+            string textoFinalizaCom = "a";
+            // When
+            bool resultado = _string.TextoTerminaCom(texto, textoFinalizaCom);
+            // Then
+            Assert.False(resultado);
         }
     }
 }
